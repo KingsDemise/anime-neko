bb817eea6508367e39625851e6cf2c0156338d10
Kingshuk Kirtania <bttb.kingshukk@gmail.com>
2023-07-07 17:42:57 +0530

    center

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { useContext, useState} from 'react';
-import axios from 'axios';
-import { Card, Image, Input, Button, Grid, Icon } from 'semantic-ui-react';
-import { AppContext } from './AppContext';
-import { Link } from 'react-router-dom/cjs/react-router-dom.min';
-import "./page.css"
-import "./App.css"
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { Card, Image, Input, Button, Grid, Icon } from "semantic-ui-react";
+import { AppContext } from "./AppContext";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./page.css";
+import "./App.css";
 
 const ParentComponent = () => {
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [searchResults, setSearchResults] = useState([]);
-  const { addToWatchlist, removeFromWatchlist, watchlist } = useContext(AppContext);
+  const { addToWatchlist, removeFromWatchlist, watchlist } =
+    useContext(AppContext);
   const searchAnime = async () => {
-    if (query.trim() === '') return;
+    if (query.trim() === "") return;
     try {
-      const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}&sfw`);
+      const response = await axios.get(
+        `https://api.jikan.moe/v4/anime?q=${query}&sfw`
+      );
       setSearchResults(response.data.data);
       console.log(response.data.data);
     } catch (error) {
@@ -41,43 +44,57 @@ const ParentComponent = () => {
   };
 
   return (
-    <div className='grid'>
-<Grid verticalAlign="middle" textAlign="center" columns={'equal'} >
-          <Grid.Column>
-          <Image style={{padding:"10px"}} verticalAlign="middle" src="https://rb.gy/ek868" circular size='small'/>
-          <h2 style={{ fontSize: '50px' ,fontWeight: 'bold',
-fontStyle: 'italic' ,fontFamily: "Helvetica, sans-serif"}}>Anime Neko</h2>
-<Link to="/watchlist" style={{ color:"#123c69",fontSize: '20px'}} ><Icon name='list alternate'/>Watchlist</Link>
+    <div className="grid">
+      <Grid verticalAlign="middle" textAlign="center" columns={"equal"}>
+        <Grid.Column>
+          <Image
+            style={{ padding: "10px" }}
+            verticalAlign="middle"
+            src="https://us-tuna-sounds-images.voicemod.net/41e56e6c-b8cd-4cec-a287-10f05fc02e1d-1687027894062.jpg"
+            circular
+            size="small"
+          />
+          <h2
+            style={{
+              fontSize: "50px",
+              fontWeight: "bold",
+              fontStyle: "italic",
+              fontFamily: "Helvetica, sans-serif",
+            }}
+          >
+            Anime Neko
+          </h2>
+          <Link to="/watchlist" style={{ color: "#123c69", fontSize: "20px" }}>
+            <Icon name="list alternate" />
+            Watchlist
+          </Link>
           <h2>Search Anime</h2>
-        <Input
-          placeholder="Enter anime name..."
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          action={<Button onClick={searchAnime}>Search</Button>}
-          style={{padding:"10px"}}
-        />
-        <Card.Group >
-          {searchResults.map((anime) => (
-            <Card key={anime.mal_id} className='cards'>
-              <Card.Content>
-              <a href={anime.url}><Image src={anime.images.jpg.image_url} alt={anime.title}/></a>
-              </Card.Content>
-              
-              <Card.Content extra>
-              
-                <Card.Header>{anime.title}</Card.Header>
+          <Input
+            placeholder="Enter anime name..."
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+            action={<Button onClick={searchAnime}>Search</Button>}
+            style={{ padding: "10px" }}
+          />
+          <Card.Group style={{ display: "flex", justifyContent: "center" }}>
+            {searchResults.map((anime) => (
+              <Card key={anime.mal_id} className="cards">
+                <Card.Content>
+                  <a href={anime.url}>
+                    <Image src={anime.images.jpg.image_url} alt={anime.title} />
+                  </a>
                 </Card.Content>
+
                 <Card.Content extra>
-                {renderActionButton(anime)}
-              </Card.Content>
-            </Card>
-          ))}
-        </Card.Group>
-          </Grid.Column>
-        </Grid>
+                  <Card.Header>{anime.title}</Card.Header>
+                </Card.Content>
+                <Card.Content extra>{renderActionButton(anime)}</Card.Content>
+              </Card>
+            ))}
+          </Card.Group>
+        </Grid.Column>
+      </Grid>
     </div>
-        
-    
   );
 };
 
